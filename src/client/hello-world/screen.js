8ef0bd8d2c3261dfea6550286b09dd53f5569dc3
Denis <45343883+denisanzora@users.noreply.github.com>
2019-03-24 16:19:22 -0700

    Update screen.js

--- src/client/hello-world/screen.js
+++ src/client/hello-world/screen.js
@@ -1,146 +1,99 @@
 import React from "react";
-
 import {
   Page,
   Container,
   VBox,
   BannerHeader,
-  BasicSegment,
   AekReactRouter,
-  Header,
-  Input,
   RouterView,
-  Link,
   Button,
-  Divider,
-  SectionDivider,
-  HeaderDivider,
-  FittedDivider,
-  HiddenDivider,
-  VerticalDivider,
-  CBox,
   Segment,
-  HBox,
-  
-  
+  Listview,
+  ListviewItem as Item,
+  panel,
+
 } from "@ombiel/aek-lib";
-
-class tile
-{
-  constructor(image, name, distance, URL)
-  {
-    
-    this.image = image;
-    this.name = name;
-    this.distance = distance;
-    this.URL = URL;
-    
-  }
-}
-function swap(array, i, j) {
-  var temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-}
-
-//tile data block
-var t = new tile("","",100,"");
-var t2 = new tile("","",1,"");
-var t3 = new tile("","",10,"");
-var listTile = [t,t2,t3];
-
-
-function getDist(tile)
-{
-  return tile.distance;
-}
-function getImage(tile)
-{
-  return tile.image;
-}
-function getName(tile)
-{
-  return tile.name;
-}
-function getURL(tile)
-{
-  return tile.URL;
-}
-
-function bubbleSort(array)
-{
-  for(var i = 0; i < array.length; i++) {
-    for(var j = 1; j < array.length; j++) {
-      if(getDist(array[j - 1]) > getDist(array[j])) {
-        swap(array, j - 1, j);
-      }
-    }
-  }
-  return array;
-}
-
-
- bubbleSort(listTile);
 
 
  const router = new AekReactRouter({useHash:false});
 
 export default class Homescreen extends React.Component {
-  
+
   componentDidMount() {
 
   }
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
 
   render() {
 
-    
+
     return (
-      
+// main menu interface =]
+      <Container>
+  <div>
+  <RouterView router={router}>
+  <Page path="/" style={{backgroundColor:"#e5e1d6"}}>
+    <Segment style={{height:"530px"}}>
+    
+      <VBox>
+        
+        <a href = "#/rda">
+        <img src="https://portal-na.campusm.exlibrisgroup.com/assets/campusM/Hackathon5/eat-live.jpg" alt="HTML tutorial" width="100%px" height="99%px"></img></a>
+        <a href = "#/non"> 
+        <img src="https://portal-na.campusm.exlibrisgroup.com/assets/campusM/Hackathon5/wsu-vendors.jpg" alt="HTML tutorial" width="100%px" height="99%px"></img></a>
+      </VBox>
+    </Segment>
+    </Page>
+            <RDA path="/rda"/>
+            <NonRDA path="/non"/>
+    </RouterView>
+  </div>
+)
+      </Container>
+    );
+  }
+}
+export class RDA extends React.Component {
+
+  componentDidMount() {
+
+  }
+// RDA Dining Halls Interface <3
+  render() {
+    //var array = bubbleSort(listTile);
+    return (
+
       <Container>
         <VBox>
-          
-          
-          <RouterView router={router}>
-            <Page path="/">
-
-
-            <BannerHeader theme="alt" key="header" data-flex={0}>RDA Cafeterias</BannerHeader>
-
-              <Segment style={{height:"200px"}}>
-      <HBox>
-        <CBox>
-          <BasicSegment>
-              <p>hi</p>
-              <Button href = "#/new">click me</Button>
-          </BasicSegment>
-          </CBox>
-        <VerticalDivider size="0"></VerticalDivider>
-        <CBox>
-        <BasicSegment>
-              <p>hi</p>
-              <Button href = "#/new">click me</Button>
-          </BasicSegment>
-        </CBox>
-      </HBox>
-    </Segment>
-
-            </Page>
-            <Example path="/new"/>
-          </RouterView>
-         
-          
-          
+        <Page path="#/rda" style={{backgroundColor:"#e5e1d6"}}>
+          <BannerHeader style={{backgroundColor:"#981e32"}} key="header" data-flex={0}><span style={{color: "white", textAlign:"center"}}>Dining Halls</span></BannerHeader>
+           <Listview>
+           <Item heading="Northside Café" icon="food" href="http://dining.wsu.edu/dining-options/northside-cafe">You are __ feet away.</Item>
+           <Item heading="Hillside Café" icon="food" href="http://dining.wsu.edu/dining-options/hillside-cafe">You are __ feet away.</Item>
+           <Item heading="Southside Café" icon="food" href="http://dining.wsu.edu/dining-options/southside-cafe">You are __ feet away.</Item>
+           <Item heading="Carlita's" icon="food" href="https://dining.wsu.edu/dining-options/carlitas/">You are __ feet away.</Item>
+           </Listview>
+           <div style={{textAlign: "center"}}>
+           <Button icon="home" basic><a href= "#/">Home</a></Button>
+           </div>
+          </Page>
         </VBox>
-        
+
       </Container>
-      
+
     );
 
   }
 
 }
-export class Example extends React.Component {
-  
+export class NonRDA extends React.Component {
+
   componentDidMount() {
 
   }
@@ -151,35 +104,35 @@ export class Example extends React.Component {
       
       <Container>
         <VBox>
-        <Page path="#/new" >
-          <BannerHeader theme="alt" key="header" data-flex={0}>RDA</BannerHeader>
-         
-           <BasicSegment>
-            
-            <p>this is {getDist(listTile[0])}</p>
-            <p>this is {getDist(listTile[1])}</p>
-            <p>this is {getDist(listTile[2])}</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-            <p>Sed posuere consectetur est at lobortis.</p>
-          </BasicSegment>
+        <Page path="#/non" style={{backgroundColor:"#e5e1d6"}}>
+          <BannerHeader style={{backgroundColor:"#981e32"}} key="header" data-flex={0}><span style={{color: "white", textAlign:"center"}}>Vendors</span></BannerHeader>
+           <Listview>
+           <Item heading="Freshens" icon="food" href="https://chinook.wsu.edu/dining/freshens/">You are __ feet away.</Item>
+           <Item heading="Gridiron" icon="food" href="http://www.gridiron-burgers.com">You are __ feet away.</Item>
+           <Item heading="Panda Express" icon="food" href="http://www.pandaexpress.com">You are __ feet away.</Item>
+           <Item heading="Pizza Pipeline" icon="food" href="http://web.pizzapipeline.com/">You are __ feet away.</Item>
+           <Item heading="Subway" icon="food" href="http://local.subway.com/FWPCons/frmSiteMainPage.aspx?CC=USA&LC=ENG&SS=45681-0">You are __ feet away.</Item>
+           </Listview>
+           <div style={{textAlign: "center"}}>
+           <Button icon="home" basic><a href= "#/">Home</a></Button>
+           </div>
           </Page>
         </VBox>
-        
       </Container>
-      
+
     );
 
   }
 
 }
-// var t = new tile("","",10.10,"");
-// var t2 = new tile("","",100,"");
-// var t3 = new tile("","",50,"");
-
-
-
-
-
-
-//list of tiles
-
+// Magic. Do not touch. Loading...
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
+// Magic. Do not touch.
